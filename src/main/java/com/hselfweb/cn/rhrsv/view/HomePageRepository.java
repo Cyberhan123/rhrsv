65685e0bc438535063948051c1409c5a6fb4a5c9
Cyberhan <255542417@qq.com>
2019-01-17 16:29:29 +0800

    updata spring boot,reset project structs

--- src/main/java/com/hselfweb/cn/rhrsv/view/HomePageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hselfweb.cn.rhrsv.view;
-
-
-import com.hselfweb.cn.rhrsv.view.HomePage;
-import org.springframework.data.repository.CrudRepository;
-
-public interface HomePageRepository extends CrudRepository<HomePage, Long> {
-}
